3ca62d8f19cb2f46ab17c508865aef84739e770b
januszewskijanuszek <74194165+januszewskijanuszek@users.noreply.github.com>
2021-03-14 11:28:30 +0100

    Add files via upload

---
+++ zadDom/Prac5.java
@@ -0,0 +1,38 @@
+package pl.edu.pjwstk.zadDom;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Prac5 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int fight = random.nextInt(3);
+        //System.out.println(fight);
+        // 0 - kamień
+        // 1 - papier
+        // 2 - norzyce
+        int chose = scanner.nextInt();
+        if(chose == fight){
+            System.out.println("Remis");
+        }
+        else if (chose == 0 && fight == 1){
+            System.out.println("Przegrana");
+        }
+        else if (chose == 0 && fight == 2){
+            System.out.println("Wygrana");
+        }
+        else if (chose == 1 && fight == 0){
+            System.out.println("Wygrana");
+        }
+        else if (chose == 1 && fight == 2){
+            System.out.println("Przegrana");
+        }
+        else if (chose == 2 && fight == 0){
+            System.out.println("Przegrana");
+        }
+        else if (chose == 2 && fight == 1){
+            System.out.println("Wygrana");
+        }
+    }
+}
